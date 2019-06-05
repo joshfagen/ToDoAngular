3ad495bd6cec858f45e233e94ba949109b759614
Josh Fagen <joshmfagen@gmail.com>
2019-06-05 18:44:46 -0400

    Working on ToDo Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './views/to-do-list/to-do-list.component';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import {
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatCardModule,
+  MatListModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatIconModule, MatInputModule, MatDatepickerModule, MatNativeDateModule
+} from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +27,20 @@ import { ToDoListComponent } from './views/to-do-list/to-do-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatListModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/to-do-list/to-do-list.component.ts
+++ src/app/views/to-do-list/to-do-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ToDoService} from '../../models/to-do.service';
+import {MatBottomSheet} from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {Task} from '../../models/Task';
 
 @Component({
   selector: 'app-to-do-list',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent implements OnInit {
-
-  constructor() { }
+  tasks: Task[];
+  completed: Task[];
+  constructor(private toDoServ: ToDoService,
+              private dialog: MatDialog,
+              private sheet: MatBottomSheet) { }
 
   ngOnInit() {
+    this.toDoServ.getTasks().subscribe(res => this.tasks = res);
+    this.toDoServ.getCompleted().subscribe(res => this.completed = res);
   }
 
+  addDialog() {
+
+  }
+
+  editDialog(i: number) {
+
+  }
+  // Function to move task to complete
+  complete(i: number) {
+    this.toDoServ.completeTask(i);
+  }
+
+  // Function to delete task
+  delete(i: number, completed) {
+    this.toDoServ.deleteTask(i, completed);
+  }
+
+  // Function to move a completed task back to to-do list
+  uncomplete(i: number) {
+    this.toDoServ.uncompleteTask(i);
+  }
 }
